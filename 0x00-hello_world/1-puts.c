db2a1cbc250d16c1dc30d7d30822ec2c79bd53fe
Zivilevs <zivileval@yahoo.com>
2021-03-27 00:53:32 -0300

    docs update

--- 0x00-hello_world/1-puts.c
+++ 0x00-hello_world/1-puts.c
@@ -1,7 +1,9 @@
+/* puts function */
+
 #include <stdio.h>
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
