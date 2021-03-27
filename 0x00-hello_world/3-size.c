642afae9fea30ee3c06c48c9e20f574ccafa98ff
Zivilevs <zivileval@yahoo.com>
2021-03-27 11:20:33 -0300

    update

--- 0x00-hello_world/3-size.c
+++ 0x00-hello_world/3-size.c
@@ -1,4 +1,4 @@
-/**
+/*uutitlksdfj;vz\aQ3<F5>689O;]. ;*
   * main - entry point
   * Return: always 0, success
   */
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %1.lld byte(s)\n", sizeof(long long int));
+	printf("Size of a long long int: %1.ld byte(s)\n", sizeof(long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
