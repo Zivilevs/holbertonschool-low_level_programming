2650d7f5c8edde88f336b6f6bdeab2dbbdfe2ad1
Zivilevs <zivileval@yahoo.com>
2021-03-27 10:03:08 -0300

    sizeof function

--- 0x00-hello_world/3-size.c
+++ 0x00-hello_world/3-size.c
@@ -11,12 +11,10 @@ int main(void)
 	int i;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", (long)sizeof(i));
 	printf("Size of a long long int: %lld byte(s)\n", (long long)sizeof(i));
-	printf("Size of a float: %lu byte(s)\n", sizeof(f));
-	fprintf();
-	
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
