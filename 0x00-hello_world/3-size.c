153ade55155175d5349b3f6fc120f54893d19ffb
Zivilevs <zivileval@yahoo.com>
2021-03-27 09:37:45 -0300

    sizeof function

---
+++ 0x00-hello_world/3-size.c
@@ -0,0 +1,21 @@
+/**
+  * main - entry point
+  * Return: always 0, success
+  */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+	int i;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", (long)sizeof(i));
+	printf("Size of a long long int: %lld byte(s)\n", (long long)sizeof(i));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+
+	return (0);
+}
