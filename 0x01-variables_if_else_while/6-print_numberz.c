3b0efa5b366c2cb3aec0675dcd967f60beacf29d
Zivilevs <zivileval@yahoo.com>
2021-03-27 23:21:40 -0300

    putchar prints numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+/**
+ * main - entry point
+ * Return: always 0, success
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int sk;
+
+	sk = 0;
+	for (; sk < 10; sk++)
+	{
+		putchar('0' + (sk % 10));
+	}
+	printf("\n");
+	return (0);
+}
