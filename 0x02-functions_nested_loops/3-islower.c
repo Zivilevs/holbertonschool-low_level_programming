e0ddfaa8cdc44d8a22fa5e3378a263e5196d5781
Zivilevs <zivileval@yahoo.com>
2021-03-31 20:38:58 -0300

    header file and if statement

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+/**
+ * _islower - a function that checks for lowercase character.
+ * Member c: is integer
+ * Return: 1 if true and 0 if false *
+ * You can only use _putchar twice in your code.
+ * c - is integer
+ */
+
+#include "holberton.h"
+
+int _islower(int c)
+{
+	if ((c > 96) && (c < 123))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
