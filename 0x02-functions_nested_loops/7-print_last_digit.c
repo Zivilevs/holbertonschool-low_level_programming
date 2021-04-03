347fec67ad0880dcb976e4e78cf79dce16aeab03
Zivilevs <zivileval@yahoo.com>
2021-04-02 23:26:53 -0300

    Style update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,8 @@
 /**
  * print_last_digit -  a function that prints the last digit of a number.
- * Using _putchar, which basiclly is write(). 1. Consider only absolute numbers.
- * 2. modulus for last digit, if n negative think of INT_MIN and avoid overflow. 3. c captures decimal number for ascii character,
+ * Using _putchar, which basiclly is write(). 1. Consider only absolute
+ * numbers. 2. modulus for last digit, if n negative think of INT_MIN
+ * and avoid overflow. 3. c captures decimal number for ascii character,
  * that _putchar prints to stdout.
  * @n: is integer
  * Return: decimal for ascii character.
@@ -17,7 +18,7 @@ int print_last_digit(int n)
 	if (n < 0)
 	{
 		a = ((n + 1) * -1);
-		b = ((a % 10) +1);
+		b = ((a % 10) + 1);
 	}
 	else
 	{
