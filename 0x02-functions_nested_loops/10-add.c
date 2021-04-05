dfd647e5335c93302668ff875b5567e9dde19434
Zivilevs <zivileval@yahoo.com>
2021-04-04 22:26:30 -0300

    last digit and ascii characters to integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+/**
+ * add - a function that adds 2 integers and returns the result.
+ * @a:first number
+ * @b:second number
+ * Return: integer
+ */
+
+#include "holberton.h"
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
+
