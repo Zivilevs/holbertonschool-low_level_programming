a7c5abf750686bf33e5fd923a05eed6aa0c35f3d
Zivilevs <zivileval@yahoo.com>
2021-04-04 22:57:54 -0300

    style update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
  * print_to_98 - a function that prints all
  * natural numbers from n to 98.
  * @n:number
- * Return: 
+ * Return:
  */
 
 #include "holberton.h"
@@ -10,9 +10,9 @@
 
 void print_to_98(int n)
 {
-	for (; n <=98; n++)
-	{	
-		n < 98 ? printf("%d, ", n):
+	for (; n <= 98; n++)
+	{
+		n < 98 ? printf("%d, ", n) :
 			printf("%d\n", n);
 	}
 
