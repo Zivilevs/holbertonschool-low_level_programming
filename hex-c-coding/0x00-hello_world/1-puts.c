14f3c0e169682a1aaa78dfa79be2f11a6854f253
Zivilevs <zivileval@yahoo.com>
2021-04-05 22:13:45 -0300

    updates dir hello-world

---
+++ hex-c-coding/0x00-hello_world/1-puts.c
@@ -0,0 +1,13 @@
+/**
+ * main - entry point
+ *
+ * Return: always 0, success
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ hex-c-coding/0x00-hello_world/3-size.c
@@ -0,0 +1,16 @@
+/**
+  * main - entry point
+  * Return: always 0, success
+  */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
